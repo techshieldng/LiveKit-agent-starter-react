b7c5dff1980ddda7a49253b885b796e2f3288293
techshieldng <infoclick.ng@gmail.com>
2025-06-29 13:44:43 +0100

    Update app-config.ts

--- app-config.ts
+++ app-config.ts
@@ -1,9 +1,9 @@
 import type { AppConfig } from './lib/types';
 
 export const APP_CONFIG_DEFAULTS: AppConfig = {
-  companyName: 'LiveKit',
-  pageTitle: 'Voice Assistant',
-  pageDescription: 'A voice assistant built with LiveKit',
+  companyName: 'Voltcom Technologies LTD',
+  pageTitle: 'VICTORIA',
+  pageDescription: 'Your Private home lesson teacher developed by Voltcom Technologies LTD',
 
   supportsChatInput: true,
   supportsVideoInput: true,
